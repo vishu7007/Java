63b82477d80e3b1ce400edf29b5f0496ad28223b
Vishav <vishavbansal007@gmail.com>
2022-07-12 17:42:06 +0530

    Day1.2

--- basic.java
+++ basic.java
@@ -1,5 +1,12 @@
 public class basic {
+    // If else method to write
     public static void main(String[] args) {
-        System.out.println("HEllo");
+        int n = 10;
+        if (n == 11) {
+            System.out.println("HEllo");
+        } else {
+            System.out.println("bye");
+        }
+
     }
 }
